5b15a16ab766e70183f87eaf2bdbac276f384411
shaynakaplan <146883115+shaynakaplan@users.noreply.github.com>
2023-10-03 12:54:32 -0700

    Add files via upload
    We've now added a program file.

---
+++ hello-world.py
@@ -0,0 +1,16 @@
+#this program will write 'Hello World'
+
+#this function tells the computer 
+#how to print 'Hello World'
+def PrintHelloWorld():
+	print("Hello World")
+
+#this defines out main() 
+#function for our program
+def main():
+	PrintHelloWorld()
+
+#when we run the program, 
+#this executes first
+if __name__=="__main__":
+	main()
