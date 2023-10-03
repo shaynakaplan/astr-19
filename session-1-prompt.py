51a0c06482a259fc83de7f392aa41745f9fb616a
Shayna Kaplan <146883115+shaynakaplan@users.noreply.github.com>
2023-10-03 15:58:26 -0700

    Session 1 prompt for Coding Journal 1.

---
+++ session-1-prompt.py
@@ -0,0 +1,20 @@
+#This program will print out
+#my name, favorite movie, and
+#favorite food.
+
+#This function prints out all
+#of the statements about me.
+def AboutMe():
+	print("My name is Shayna Kaplan.")
+	print("My favorite movie is Twilight.")
+	print("My favorite food is soup.")
+
+#This defines the main() function
+#for the program.
+def main():
+	AboutMe()
+
+#This executes first when we run
+#the program
+if __name__=="__main__":
+	main()
