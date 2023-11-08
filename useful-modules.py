bd87d1228e50049cc9a7341a0e0a8d96721c41f7
Shayna Kaplan <146883115+shaynakaplan@users.noreply.github.com>
2023-11-07 16:55:13 -0800

    Useful Python modules

---
+++ useful-modules.py
@@ -0,0 +1,10 @@
+import numpy as np 					#Numpy library
+import matplotlib.pyplot as plt		#Matplotlib pyplot
+
+import sys				#gives access to a C-like sys library
+import os				#gives access to operating system
+
+#prints any command line arguments, incl prgram name
+print(sys.argv)	
+#print the current working directory
+print(os.getcwd())
